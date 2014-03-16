b12a356376603d97b3471a7173e61d02c54e9fd2
kongaraju <kongaraju@gmail.com>
2014-03-16 17:18:03 +0530

    added pages

--- js/app.js
+++ js/app.js
@@ -36,5 +36,12 @@ $(function () {
         });
         
         
+        
+        $('.navbar .dropdown').hover(function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideToggle(400);
+            }, function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideToggle(400)
+            });
+        
     
     });
